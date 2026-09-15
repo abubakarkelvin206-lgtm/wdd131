f1a9f271fb7d5c9600e3f15d4497b13391ad6620
Abubakar Kelvin <Abubakarkelvin206@gmail.com>
2026-09-15 13:36:29 +0100

    wagwan

---
+++ scripts/place.js
@@ -0,0 +1,22 @@
+const today = new Date();
+
+document.getElementById("currentyear").textContent = today.getFullYear();
+
+
+document.getElementById("lastModified").textContent =
+    `Last Modified: ${document.lastModified}`;
+
+
+    function calculateWindChill(temp, wind) {
+         return 13.12 + (0.6215 * temp) - (11.37 * wind ** 0.16) + (0.3965 * temp * wind ** 0.16);
+}
+
+const temperature = 28;
+const windSpeed = 10;
+
+if (temperature <= 10 && windSpeed > 4.8) { document.getElementById("windchill").textContent =
+    calculateWindChill(temperature, windSpeed).toFixed(1) + " °C";
+    
+}
+
+    
