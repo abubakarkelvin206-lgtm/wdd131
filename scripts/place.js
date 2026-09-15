ecd078597ae98f9873ec36d89f161da4d6114283
Abubakar Kelvin <Abubakarkelvin206@gmail.com>
2026-09-15 16:37:50 +0100

    uk

--- scripts/place.js
+++ scripts/place.js
@@ -11,7 +11,7 @@ document.getElementById("lastModified").textContent =
          return 13.12 + (0.6215 * temp) - (11.37 * wind ** 0.16) + (0.3965 * temp * wind ** 0.16);
 }
 
-const temperature = 28;
+const temperature = 5;
 const windSpeed = 10;
 
 if (temperature <= 10 && windSpeed > 4.8) { document.getElementById("windchill").textContent =
